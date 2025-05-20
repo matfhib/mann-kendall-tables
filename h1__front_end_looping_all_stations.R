f17fc6948ad8a63b1675fdbea5028bbe90d00acd
Hibbs <89001005653030@fedidcard.gov>
2025-05-20 11:02:56 -0400

    updated

---
+++ h1__front_end_looping_all_stations.R
@@ -0,0 +1,22 @@
+
+library(Kendall)
+setwd('Q:/WRRP/Working Files_Individuals/Mattie Hibbs/Statistics/MK Tables/Data')
+df_raw <- read.csv('RER_2025_groundwater_reduced.csv')
+mcl_df <- read.csv('Q:/WRRP/Working Files_Individuals/Mattie Hibbs/Statistics/MK Tables/2025 RER MCLs.csv')
+
+get_full_table <- function(data, media_type, station_name, mcl_table)
+{
+  stn_list <- get_station_list(df_raw, media_type, station_name, mcl_table)
+  chemicals_list <- names(stn_list)
+  station_and_chems <- get_station_chems(stn_list, chemicals_list)
+  resdf <- get_mk_results2(station_and_chems)
+  return(resdf)
+}
+
+
+
+resdf[order(resdf$Anagroup), ]
+stn_list <- get_station_list(df_raw, "UF", "GW-078", mcl_table)
+
+
+biglist <- get_station_list2(df_raw, c("F","UF"), "GW-078",mcl_df)
